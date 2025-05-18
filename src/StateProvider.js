55b649ff5fa13985963d4a08deccefcdfbda5188
LUNA <shenbagapriya0923@gmail.com>
2025-05-18 20:45:37 +0930

    Update StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,14 +1,11 @@
 import React, { createContext, useReducer, useContext } from 'react';
 
-// Create Context
 export const StateContext = createContext();
 
-// State Provider Component
 export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>
 );
 
-// Custom Hook to access State Value
 export const useStateValue = () => useContext(StateContext);
